67c05fff100e89f2d8fa609cfbbc38ac93006110
Lea <lea.henry5@hotmail.fr>
2025-02-18 20:04:42 +0000

    songs links

--- src/data/songs.js
+++ src/data/songs.js
@@ -7,7 +7,7 @@ const songs = [
       album: "Meddle",
       albumCover:
         "https://api.deezer.com/album/12114236/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/7/f/2/0/7f26817e23728a9c7cd05de212f4bac6.mp3?hdnea=exp=1739141220~acl=/api/1/1/7/f/2/0/7f26817e23728a9c7cd05de212f4bac6.mp3*~data=user_id=0,application_id=42~hmac=51dbb82c50b8e1b47892549c2d158b3b3f85111491e42bad1598322666946ca0",
+      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/7/f/2/0/7f26817e23728a9c7cd05de212f4bac6.mp3?hdnea=exp=1739909476~acl=/api/1/1/7/f/2/0/7f26817e23728a9c7cd05de212f4bac6.mp3*~data=user_id=0,application_id=42~hmac=ee80ad897c99e36a98867532630abf60d90f8b218b766039de486001516a3543",
     },
     {
       id: 2,
@@ -16,7 +16,7 @@ const songs = [
       album: "The Division Bell",
       albumCover:
         "https://api.deezer.com/album/12114254/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/c/a/7/0/ca76d30dc564bdd7e21ef634c715ce28.mp3?hdnea=exp=1739141875~acl=/api/1/1/c/a/7/0/ca76d30dc564bdd7e21ef634c715ce28.mp3*~data=user_id=0,application_id=42~hmac=8207d388b54f3a3768d96bb8a70c2b8740985857f7a71c4eca8ccf980fd04352",
+      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/c/a/7/0/ca76d30dc564bdd7e21ef634c715ce28.mp3?hdnea=exp=1739909573~acl=/api/1/1/c/a/7/0/ca76d30dc564bdd7e21ef634c715ce28.mp3*~data=user_id=0,application_id=42~hmac=929fe394623361507ed790e6db0f2a14d19d6594f26b1f474058215b877e7a84",
     },
     {
       id: 3,
@@ -25,16 +25,16 @@ const songs = [
       album: "Yesterwynde",
       albumCover:
         "https://api.deezer.com/album/575404841/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/f/e/0/0/fe079275f1b3b745b94ee13a4daa1656.mp3?hdnea=exp=1739141974~acl=/api/1/1/f/e/0/0/fe079275f1b3b745b94ee13a4daa1656.mp3*~data=user_id=0,application_id=42~hmac=f7ac77c736f894e4dcb568a6c7f7b44d3d50ed7916a49925cbfed4eca2070b1c",
+      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/f/e/0/0/fe079275f1b3b745b94ee13a4daa1656.mp3?hdnea=exp=1739909610~acl=/api/1/1/f/e/0/0/fe079275f1b3b745b94ee13a4daa1656.mp3*~data=user_id=0,application_id=42~hmac=4308bfce13ea81648b5fd75fdcca7ee50985175424f52e0cfb4672594e392553",
     },
     {
       id: 4,
-      title: "Tueur De Fleur",
+      title: "Tueur De Fleurs",
       artist: "La Femme",
       album: "Mystère",
       albumCover:
         "https://api.deezer.com/album/13867422/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/c/0/b/0/c0bd3e5529abbc540b51b193fcc6fbd9.mp3?hdnea=exp=1739142040~acl=/api/1/1/c/0/b/0/c0bd3e5529abbc540b51b193fcc6fbd9.mp3*~data=user_id=0,application_id=42~hmac=795046031a40e3bec9aaf78db80e0280a85a9ad383c10fb9f0c34005ed746d52",
+      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/c/0/b/0/c0bd3e5529abbc540b51b193fcc6fbd9.mp3?hdnea=exp=1739909638~acl=/api/1/1/c/0/b/0/c0bd3e5529abbc540b51b193fcc6fbd9.mp3*~data=user_id=0,application_id=42~hmac=d661920e8cc964e47f379a1b4666b2d48527f35dba00497ea179777c51666b1d",
     },
     {
       id: 5,
@@ -43,7 +43,7 @@ const songs = [
       album: "Talkie Walkie",
       albumCover:
         "https://api.deezer.com/album/320430/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/a/1/b/0/a1bc811f3d368b58a5260209c5dd92e1.mp3?hdnea=exp=1739142151~acl=/api/1/1/a/1/b/0/a1bc811f3d368b58a5260209c5dd92e1.mp3*~data=user_id=0,application_id=42~hmac=bae8b8950f14b489a3c1e047336bacd01d3e9c7336a6279888dccd15a753ef75",
+      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/a/1/b/0/a1bc811f3d368b58a5260209c5dd92e1.mp3?hdnea=exp=1739909702~acl=/api/1/1/a/1/b/0/a1bc811f3d368b58a5260209c5dd92e1.mp3*~data=user_id=0,application_id=42~hmac=cc1c3a81b20863b983ebe676426af3661f3d77d2443500cfadf9fd2f52e774a8",
     }
     
   ];
