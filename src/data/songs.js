756076854b708431c13c6d1b870dd591d8d770e3
Lea <lea.henry5@hotmail.fr>
2025-02-23 18:01:29 +0000

    songs links

--- src/data/songs.js
+++ src/data/songs.js
@@ -7,7 +7,7 @@ const songs = [
       album: "Meddle",
       albumCover:
         "https://api.deezer.com/album/12114236/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/7/f/2/0/7f26817e23728a9c7cd05de212f4bac6.mp3?hdnea=exp=1739911867~acl=/api/1/1/7/f/2/0/7f26817e23728a9c7cd05de212f4bac6.mp3*~data=user_id=0,application_id=42~hmac=4cfd95ce3266f78095cce971fc04105821a08d170cdb02d0fe33280db6ba81ff",
+      previewURL: "https://github.com/LH-90/Music_app/raw/main/public/Echoes.m4a",
     },
     {
       id: 2,
@@ -16,7 +16,7 @@ const songs = [
       album: "The Division Bell",
       albumCover:
         "https://api.deezer.com/album/12114254/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/c/a/7/0/ca76d30dc564bdd7e21ef634c715ce28.mp3?hdnea=exp=1739912069~acl=/api/1/1/c/a/7/0/ca76d30dc564bdd7e21ef634c715ce28.mp3*~data=user_id=0,application_id=42~hmac=1a17e004dc31d7a49c73d177edcd381d36c6582a5d2447c8ddfd833b7c8e867a",
+      previewURL: "https://github.com/LH-90/Music_app/raw/main/public/High%20Hopes.m4a",
     },
     {
       id: 3,
@@ -25,7 +25,7 @@ const songs = [
       album: "Yesterwynde",
       albumCover:
         "https://api.deezer.com/album/575404841/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/f/e/0/0/fe079275f1b3b745b94ee13a4daa1656.mp3?hdnea=exp=1739912106~acl=/api/1/1/f/e/0/0/fe079275f1b3b745b94ee13a4daa1656.mp3*~data=user_id=0,application_id=42~hmac=270881a859a67b8e002745d01fdcf1d05e3c4683d53ddd117e47997b6d739ce9",
+      previewURL: "https://github.com/LH-90/Music_app/raw/main/public/Perfume%20Of%20The%20Timeless.m4a",
     },
     {
       id: 4,
@@ -34,7 +34,7 @@ const songs = [
       album: "Mystère",
       albumCover:
         "https://api.deezer.com/album/13867422/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/c/0/b/0/c0bd3e5529abbc540b51b193fcc6fbd9.mp3?hdnea=exp=1739912134~acl=/api/1/1/c/0/b/0/c0bd3e5529abbc540b51b193fcc6fbd9.mp3*~data=user_id=0,application_id=42~hmac=54aa766041770ccd6eb3beea203292c585c08b7048b28d00b8ee4bae7364701b",
+      previewURL: "https://github.com/LH-90/Music_app/raw/main/public/Tueur%20De%20Fleurs.m4a",
     },
     {
       id: 5,
@@ -43,7 +43,7 @@ const songs = [
       album: "Talkie Walkie",
       albumCover:
         "https://api.deezer.com/album/320430/image",
-      previewURL: "https://cdnt-preview.dzcdn.net/api/1/1/a/1/b/0/a1bc811f3d368b58a5260209c5dd92e1.mp3?hdnea=exp=1739912165~acl=/api/1/1/a/1/b/0/a1bc811f3d368b58a5260209c5dd92e1.mp3*~data=user_id=0,application_id=42~hmac=2ea6b1ff3b3b5f3ef33e9836870716ffdc377beb9e1129aa1189cfe90756be11",
+      previewURL: "https://github.com/LH-90/Music_app/raw/main/public/Cherry%20Blossom%20Girl.m4a",
     }
     
   ];
